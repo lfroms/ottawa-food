171dd360b18bc4813cbd0e009d24b86bf83d5a08
Lukas Romsicki <lukas@romsicki.com>
2020-04-10 19:35:22 -0400

    Return existing bucket list item on create if exists

--- app/services/bucket_list_item_create_service.rb
+++ app/services/bucket_list_item_create_service.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 class BucketListItemCreateService < UseCaseService
   def execute(user_id:, restaurant_id:)
-    BucketListItem.create(user_id: user_id, restaurant_id: restaurant_id)
+    new_item = BucketListItem.create(user_id: user_id, restaurant_id: restaurant_id)
+
+    if new_item.id.nil?
+      BucketListItem.find_by(user_id: user_id, restaurant_id: restaurant_id)
+    else
+      new_item
+    end
   end
 end
