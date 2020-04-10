6a34361fd6da5d0c500c449407844374c670bdd4
Lukas Romsicki <lukas@romsicki.com>
2020-04-10 16:05:51 -0400

    Add filter by pending to bucket list items

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,6 +5,16 @@ module Types
     field :name, String, null: false
     field :email, String, null: false
     field :favorite_restaurants, RestaurantType.connection_type, null: false
-    field :bucket_list_items, BucketListItemType.connection_type, null: false
+    field :bucket_list_items, BucketListItemType.connection_type, null: false do
+      argument :show_only_pending, Boolean, required: false
+    end
+
+    def bucket_list_items(show_only_pending: false)
+      if show_only_pending
+        object.bucket_list_items.pending
+      else
+        object.bucket_list_items
+      end
+    end
   end
 end
