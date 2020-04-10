1ebf909df45cdfbccb1427e3d377a8317cb0c482
Lukas Romsicki <lukas@romsicki.com>
2020-04-09 20:43:18 -0400

    Add favorites

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
   validates :yelp_id, null: false
+
+  has_many :users, through: :favorites
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_003501) do
+ActiveRecord::Schema.define(version: 2020_04_10_004127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_favorites_on_restaurant_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "yelp_id", null: false
@@ -28,4 +37,6 @@ ActiveRecord::Schema.define(version: 2020_04_10_003501) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favorites", "restaurants"
+  add_foreign_key "favorites", "users"
 end
