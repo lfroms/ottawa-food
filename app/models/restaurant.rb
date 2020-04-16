905a0e99ec63c1ad39c058b91682f1172dcf2e0d
Lukas Romsicki <lukas@romsicki.com>
2020-04-16 12:15:53 -0400

    Replace missing sync_from_yelp job enqueuement

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class Restaurant < ApplicationRecord
+  after_create_commit :sync_from_yelp
+
   validates :yelp_id, presence: true, uniqueness: true
   validates :yelp_url, format: { with: /#{URI.regexp}/ }, allow_nil: true
   validates :phone, presence: false
@@ -15,6 +17,10 @@ class Restaurant < ApplicationRecord
 
   def in_bucket_list?(user_id:)
     bucket_list_items.exists?(user_id: user_id)
+  end
+
+  def sync_from_yelp
+    SyncRestaurantFromYelpJob.perform_later(yelp_id: yelp_id)
   end
 
   def self.find_otherwise_create(yelp_id:)
