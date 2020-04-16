b65ec20421c8ce22ae01050c73f5600aeb8f10a9
Lukas Romsicki <lukas@romsicki.com>
2020-04-15 22:56:15 -0400

    Remove unused ID field

--- app/graphql/types/recommendation_type.rb
+++ app/graphql/types/recommendation_type.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 module Types
   class RecommendationType < Types::BaseObject
-    field :id, ID, null: false
     field :restaurant, Types::RestaurantType, null: false
     field :index, Integer, null: false
     field :score, Float, null: false
